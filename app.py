1449fb12c24e7954c699d4f7ec2619f8f48c1942
anurag-verma <anuragverma394@gmail.com>
2023-07-08 18:05:40 +0530

    added

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+import time
+import requests
 import pandas as pd
 import numpy as nprun 
 import streamlit as st
 import plotly.express as px
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://assets9.lottiefiles.com/packages/lf20_leneywe2.json"
+
+lottie_hello = load_lottieurl(lottie_url_hello)
+
+st_lottie(lottie_hello, key="Welcome")
 
 #loading data
 @st.cache_data
@@ -17,10 +34,12 @@ with st.spinner('Loading data...'):
 #create a title for your app
 st.title('House price data analysis')
 
+
 #display the data
 if st.checkbox('Show dataset',True):
      st.subheader('Dataset')
      st.dataframe(df)
 
 #app
+
      
